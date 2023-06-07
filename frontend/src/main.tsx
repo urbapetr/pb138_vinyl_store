b093c3487852da09c7a288f1872e7b2f2b1ea163
@x496353 <x496353@fi.muni.cz>
2023-06-07 22:42:30 +0200

    cleanup: remove web vitals

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
-import reportWebVitals from './utils/reportWebVitals';
 
 import './index.css';
 
@@ -18,5 +17,3 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     </QueryClientProvider>
   </React.StrictMode>
 );
-
-reportWebVitals();
