ad97d9981c57bdef28c6e49b47488a751ee8f5ba
Petr Urbanek <xurbane3@fi.muni.cz>
2023-06-07 13:06:48 +0200

    feat: Search bar on top now redirect by search param

--- frontend/src/components/TopBar/SearchBar.tsx
+++ frontend/src/components/TopBar/SearchBar.tsx
@@ -1,15 +1,32 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 export function SearchBar() {
+  const [searchInput, setSearchInput] = useState('');
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchInput(event.target.value);
+  };
+
   return (
     <form className="flex items-center justify-center">
       <div className="join">
         <div>
           <div>
-            <input className="input join-item" placeholder="Search..." />
+            <input
+              className="input join-item"
+              placeholder="Search..."
+              value={searchInput}
+              onChange={handleInputChange}
+            />
           </div>
         </div>
-        <button type="submit" className="btn join-item">
+        <Link
+          to={`/search?needle=${encodeURIComponent(searchInput)}`}
+          className="btn join-item"
+        >
           Search
-        </button>
+        </Link>
       </div>
     </form>
   );
