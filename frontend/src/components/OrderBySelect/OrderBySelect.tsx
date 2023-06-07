4d3f7b5f79c77a35705ff5e7742d84b749124c57
@x496353 <x496353@fi.muni.cz>
2023-06-07 21:49:12 +0200

    feat: orderby

--- frontend/src/components/OrderBySelect/OrderBySelect.tsx
+++ frontend/src/components/OrderBySelect/OrderBySelect.tsx
@@ -1,5 +1,14 @@
 import { useSearchParams } from 'react-router-dom';
 
+const map = new Map([
+  ['title', 'title_asc'],
+  ['title inverse', 'title_desc'],
+  ['artist', 'artist_asc'],
+  ['artist inverse', 'artist_desc'],
+  ['newest', 'updatedAt_desc'],
+  ['oldest', 'updatedAt_asc'],
+]);
+
 export function OrderBySelect() {
   const [searchParams, setSearchParams] = useSearchParams();
 
@@ -8,17 +17,26 @@ export function OrderBySelect() {
       defaultValue="Order by"
       className="select select-bordered w-full max-w-xs"
       onChange={(e) => {
-        const newSearchParams: URLSearchParams = { ...searchParams };
-        newSearchParams.set('orderby', e.target.value.toLowerCase());
-        newSearchParams.set('genre', 'All');
-        setSearchParams(newSearchParams.toString());
+        const value = e.target.value.toLowerCase();
+        if (map.has(value)) {
+          const converted = map.get(value);
+          if (converted) {
+            const newSearchParams: URLSearchParams = new URLSearchParams(
+              searchParams
+            );
+            newSearchParams.set('orderby', converted);
+            setSearchParams(newSearchParams.toString());
+          }
+        }
       }}
     >
       <option disabled>Order by</option>
+      <option>Title</option>
+      <option>Title Inverse</option>
+      <option>Artist</option>
+      <option>Artist Inverse</option>
       <option>Newest</option>
       <option>Oldest</option>
-      <option>Cheapest</option>
-      <option>Most expensive</option>
     </select>
   );
 }
